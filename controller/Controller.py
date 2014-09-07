2377f31d45340ec5dcec14fe09d39e1d17874c96
Pablo <pablo.buenaposada@gmail.com>
2014-09-07 13:14:48 +0200

    added warning colors to circle class

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,19 +1,3 @@
-def updateRpm(canvas,serial,rpm):
-    rpm.setRpm(serial.getRpm())
-    canvas.after(10,updateRpm,canvas,serial,rpm)
-    
-def updateBattery(canvas,serial,circle):
-    circle.setValue(serial.getBattery())
-    canvas.after(10,updateBattery,canvas,serial,circle)
-
-def updateTps(canvas,mcp3208,channel,bar):
-    bar.setHeight(mcp3208.getADC(channel))
-    canvas.after(10,updateTps,canvas,mcp3208,channel,bar)
-    
-def updateAnalog(canvas,mcp3208,channel,circle):
-    circle.setValue(mcp3208.getADC(channel))    
-    canvas.after(10,updateAnalog,canvas,mcp3208,channel,circle)
-
 def updateAll(canvas,mcp3208,serial,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake):
     rpm.setRpm(serial.getRpm())
     speed.setText(serial.getVss())
--- gui/Circle.py
+++ gui/Circle.py
@@ -2,27 +2,36 @@ from Text import *
 
 class Circle:
     
-    def __init__(self,canvas,x,y,diameter,width,startDegrees,rangeDegrees,minValue,maxValue,color,fontSize,text):
+    def __init__(self,canvas,x,y,diameter,width,startDegrees,rangeDegrees,minValue,maxValue,minWrnValue,maxWrnValue,minColor,normalColor,maxColor,fontSize,text):
         self.canvas = canvas
         self.minValue=minValue
         self.maxValue=maxValue
         self.rangeDegrees = rangeDegrees
         self.startDegrees = startDegrees
-        self.idCircle = self.canvas.create_arc(x-(diameter/2),y-(diameter/2),x+(diameter/2),y+(diameter/2),style="arc", start=self.startDegrees, extent=-self.rangeDegrees,fill="",outline=color,width=width)
+        self.minWrnValue = minWrnValue
+        self.maxWrnValue = maxWrnValue
+        self.minColor = minColor
+        self.normalColor = normalColor
+        self.maxColor = maxColor
+        self.idCircle = self.canvas.create_arc(x-(diameter/2),y-(diameter/2),x+(diameter/2),y+(diameter/2),style="arc", start=self.startDegrees, extent=-self.rangeDegrees,fill="",outline=normalColor,width=width)
         
         self.idValue = Text(canvas,x,y,"Helvetica",fontSize,"bold","black","80")
         self.idText = Text(canvas,x,y+(diameter/4.5),"Helvetica",int(0.371*fontSize),"bold","black",text) 
 
-    def setValue(self,degreesFill):
-        if degreesFill > self.maxValue:
-           degreesFill = self.maxValue
-        elif degreesFill < self.minValue:
-            degreesFill = self.minValue
+    def setValue(self,value):
+        if value > self.maxValue:
+           value = self.maxValue
+        elif value < self.minValue:
+            value = self.minValue
         
-        degrees = (((degreesFill - self.minValue) * (self.rangeDegrees - 0)) / (self.maxValue - self.minValue)) + 0
+        degrees = (((value - self.minValue) * (self.rangeDegrees - 0)) / (self.maxValue - self.minValue)) + 0        
+        if (value > self.minWrnValue) and (value < self.maxWrnValue): color=self.normalColor
+        elif value >= self.maxWrnValue: color=self.maxColor
+        else: color=self.minColor
         
         self.canvas.itemconfig(self.idCircle, extent=-degrees)
-        self.idValue.setText(degreesFill)
+        self.canvas.itemconfig(self.idCircle, outline=color)
+        self.idValue.setText(value)
         
     
 
