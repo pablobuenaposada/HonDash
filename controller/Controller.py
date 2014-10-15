389f8860d2e9fc8f7d3b7aa4db8cf71e2ae652c5
Pablo <pablo.buenaposada@gmail.com>
2014-10-15 23:05:09 +0200

    accelerometer cleaned and some little fixes

--- controller/Controller.py
+++ controller/Controller.py
@@ -6,23 +6,33 @@ class Controller:
         self.startRecord = -1
         self.endRecord = -1
         self.timer = Time()
-    
-    def updateAll(self,canvas,mcp3208,serial,controller,digital,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake,runTime,inj,duty,vtec,iat,ign,mapp,arrowLeft,arrowRight):
 
-	rpm.setRpm(serial.getRpm())
-        speed.setText(739)#serial.getVss())
-        #oilTemp.setValue(mcp3208.getADC(7)) 
+    def things2control(self,digital27,arrowLeft):
+	self.digital27 = digital27
+	self.arrowLeft = arrowLeft	
+
+    def f2(self,channel):
+    	self.arrowLeft.setFill(self.digital27.getValue())
+    
+    def updateAll(self,canvas,mcp3208,serial,controller,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake,runTime,inj,duty,vtec,iat,ign,mapp,arrowLeft,arrowRight,accelerometer,g):
+
+	#rpm.setRpm(serial.getRpm())
+        speed.setText(120)#serial.getVss())
+        oilTemp.setValue(mcp3208.getADC(7)) 
         oilPressure.setValue(5000)#mcp3208.getADC(6))
-        #fuel.setWidth(mcp3208.getADC(7))
+        fuel.setWidth(mcp3208.getADC(7))
         h2o.setValue(5000)#mcp3208.getADC(5))
         h2oEcu.setText(int(serial.getEct()))
         battery.setText("12.5")#round(serial.getBattery(),1))
         #throttle.setHeight(mcp3208.getADC(8))
         #clutch.setHeight(mcp3208.getADC(4))
-        #brake.setHeight(mcp3208.getADC(3))
+        brake.setHeight(mcp3208.getADC(7))
         time = self.timer.getTime()
         runTime.setText(self.timer.getTimeString())
-        canvas.after(10,controller.updateAll,canvas,mcp3208,serial,controller,digital,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake,runTime,inj,duty,vtec,iat,ign,mapp,arrowLeft,arrowRight)
+	axes = accelerometer.getAxes(True)
+	g.setGforce(axes['x'],axes['y'])
+
+        canvas.after(10,controller.updateAll,canvas,mcp3208,serial,controller,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake,runTime,inj,duty,vtec,iat,ign,mapp,arrowLeft,arrowRight,accelerometer,g)
 
     def checkSpeedRecord(self,speed):
         now = self.timer.getTime()
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from gui.Gforce import *
 from controller.Controller import *
 from devices.CromeQD2 import *
 from devices.MCP3208 import *
-from devices.Digital import *
+from devices.DigitalInput import *
+from devices.ADXL345 import *
 
 #ratio constants
 root = Tk()
@@ -32,27 +33,27 @@ canvas = Canvas(root,width=winWidth,height=winHeight,bg="black")
 canvas.pack()
 
 #init graphics
-rpm = Rpm(canvas,winWidth/2,winHeight/4.0,winWidth/1.00,winHeight/2.75,100,"#ffa500","yellow",20,140,0,10000)
-speed = Text(canvas,winWidth/2,winHeight/4,"Helvetica",speedFontSize,"bold italic","white","","","137")
-speedUnit = Text(canvas,(winWidth/4)*2.5,(winHeight/4)+50,"Helvetica",15,"bold italic","white","","","km/h")
-mileage = Text(canvas,winWidth/2,(winHeight/10)*3,"Helvetica",10,"bold ","white","","","162.372 KM")
-fuel = Bar(canvas,(winWidth/2)-200,(winHeight/16)*5.75,0,400,0,25,0,4096,"#efbbff","DeepPink4")
-canvas.create_text(winWidth/2,50,text="1926",font="Helvetica 70 bold",fill="#ee9b02",stipple='gray25')
+rpm = Rpm(canvas,winWidth/2,(winHeight/4.0)+10,winWidth/1.00,winHeight/2.75,100,"#ffa500","yellow",20,140,0,10000)
+speed = Text(canvas,(winWidth/2)-15,winHeight/4,"Helvetica",speedFontSize,"bold italic","white","","","137")
+speedUnit = Text(canvas,(winWidth/4)*2.5,(winHeight/4)+43,"Helvetica",20,"bold italic","white","","","km/h")
+mileage = 1 #Text(canvas,winWidth/2,(winHeight/10)*3,"Helvetica",10,"bold ","white","","","162.372 KM")
+fuel = Bar(canvas,(winWidth/2)-200,((winHeight/16)*5.75)+3,0,400,0,25,0,4096,"#efbbff","DeepPink4")
+#canvas.create_text(winWidth/2,50,text="1926",font="Helvetica 70 bold",fill="#ee9b02",stipple='gray25')
 
 
 clutch = Bar(canvas,winWidth-182,winHeight,60,60,0,200,0,200,"blue","midnight blue")
-brake = Bar(canvas,winWidth-121,winHeight,60,60,0,200,0,200,"red","firebrick4")
+brake = Bar(canvas,winWidth-121,winHeight,60,60,0,200,2100,2400,"red","firebrick4")
 throttle = Bar(canvas,winWidth-60,winHeight,60,60,0,200,0,200,"green","dark green")
 
 oilTemp = Circle(canvas,(winWidth/8)*1,(winHeight/32)*18,225,60,240,300,0,255,200,3000,"blue","#28cfbc","red",circleValueSize,circleTextSize,"white","OIL T","gray30")
 oilPressure = Circle(canvas,(winWidth/8)*3,(winHeight/32)*18,225,60,240,300,0,255,200,3000,"blue","#28cfbc","red",circleValueSize,circleTextSize,"white","OIL P","gray30")
 h2o = Circle(canvas,(winWidth/8)*5,(winHeight/32)*18,225,60,240,300,0,255,200,3000,"blue","#28cfbc","red",circleValueSize,circleTextSize,"white","H2O T","gray30")
-g = Gforce(canvas,(winWidth/8)*7,(winHeight/32)*18,285,2,1,"gray",4,"red","white")
+g = Gforce(canvas,(winWidth/8)*7,(winHeight/32)*18,283,2,2,"gray",6,"red","white")
 
 arrowLeft = Arrow(canvas,(winWidth/3)*1,winHeight/4,0.25,"green","left")
 arrowRight = Arrow(canvas,(winWidth/3)*2,winHeight/4,0.25,"green","right")
 
-runTime = Text(canvas,(winWidth/7),(winHeight/64)*1,"Helvetica",30,"bold italic","white","Run Time ","","00:00:00")
+runTime = Text(canvas,(winWidth/7)*5,(winHeight/64)*52,"Helvetica",30,"bold italic","white","Run ","","00:00:00")
 h2oEcu = Text(canvas,(winWidth/7)*3,(winHeight/64)*52,"Helvetica",30,"bold italic","white","H2O: ","cº","44")
 inj = Text(canvas,(winWidth/7)*3,(winHeight/64)*56,"Helvetica",30,"bold italic","white","INJ: ","ms","4")
 duty = Text(canvas,(winWidth/7)*3,(winHeight/64)*60,"Helvetica",30,"bold italic","white","DTY: ","ms","4")
@@ -62,20 +63,19 @@ iat = Text(canvas,(winWidth/7)*1,(winHeight/64)*56,"Helvetica",30,"bold italic",
 ign = Text(canvas,(winWidth/7)*1,(winHeight/64)*60,"Helvetica",30,"bold italic","white","IGN: ","º","30")
 mapp = Text(canvas,(winWidth/7)*1,(winHeight/64)*64,"Helvetica",30,"bold italic","white","MAP: ","mBar","433")
 
-#init devices
-controller = Controller()
-serial = CromeQD2()
-mcp3208 = MCP3208()
-digital = Digital()
-digital.setInput(27)
-
 def f1(channel):
     arrowLeft.setFill(digital.getValue(27))
-    
-digital.addEvent(27,f1)
+
+#init devices
+serial = CromeQD2()
+mcp3208 = MCP3208()
+controller = Controller()
+digital27 = DigitalInput(27,controller.f2)    
+controller.things2control(digital27,arrowLeft)
+accelerometer = ADXL345(0.6,0.5,0)
 
 #update graphics
-canvas.after(10,controller.updateAll,canvas,mcp3208,serial,controller,digital,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake,runTime,inj,duty,vtec,iat,ign,mapp,arrowLeft,arrowRight)
+canvas.after(10,controller.updateAll,canvas,mcp3208,serial,controller,rpm,speed,oilTemp,oilPressure,h2o,h2oEcu,battery,fuel,throttle,clutch,brake,runTime,inj,duty,vtec,iat,ign,mapp,arrowLeft,arrowRight,accelerometer,g)
 
 #main loop
 root.mainloop()
