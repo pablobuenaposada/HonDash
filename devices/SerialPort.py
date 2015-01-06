e1e0641830e0d0f6701eb6f560f2ce79b484ce9e
Pablo Buenaposada <read.the.disclaimer@gmail.com>
2015-01-06 02:31:37 +0100

    Update SerialPort.py

--- devices/SerialPort.py
+++ devices/SerialPort.py
@@ -3,35 +3,40 @@ from serial import *
 class SerialPort:
 
     def __init__(self):
-	try:
+        try:
             self.serialPort = Serial('/dev/ttyAMA0',38400,timeout=0.01)
             self.serialPort.open()
-	except:
-	    pass
+        except:
+            pass
 
     def getByte(self,address):
         try:
-	    self.serialPort.flushInput()
+            self.serialPort.flushInput()
             self.serialPort.write(chr(address))
             response = self.serialPort.read(1)
             if len(response) <= 0 : return 1
             else: return ord(response)
-    	except:
-	    self.serialPort = Serial('/dev/ttyAMA0',38400,timeout=0.01)
-            self.serialPort.open()
-	    return 0
+        except:
+            try:
+                self.serialPort.close()
+                self.serialPort = Serial('/dev/ttyAMA0',38400,timeout=0.01)
+                self.serialPort.open()
+                return 1
+            except: return 1
 
     def getByteFromThree(self,address1,address2,address3):
         try:
-	    self.serialPort.flushInput()
+            self.serialPort.flushInput()
             self.serialPort.write(chr(address1))
-	    self.serialPort.write(chr(address2))
-	    self.serialPort.write(chr(address3))
+            self.serialPort.write(chr(address2))
+            self.serialPort.write(chr(address3))
             response = self.serialPort.read(1)
             if len(response) <= 0 : return 1
             else: return ord(response)
-    	except:
-	    self.serialPort = Serial('/dev/ttyAMA0',38400,timeout=0.01)
-            self.serialPort.open()
-	    return 0
- 	    
+        except:
+            try:
+                self.serialPort.close()
+                self.serialPort = Serial('/dev/ttyAMA0',38400,timeout=0.01)
+                self.serialPort.open()
+                return 1
+            except: return 1
