f3d030b71f203bd9a903d1a19af070db3921eb96
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2018-04-15 16:49:08 +0200

    testing digital inputs

--- src/bench/digital_input.py
+++ src/bench/digital_input.py
@@ -5,6 +5,7 @@ sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from devices.digital_input import DigitalInput
 
+di = DigitalInput(22, None)
+
 while True:
-    di = DigitalInput(22, None)
     print(di.value())
