def586f3e05473c3f5f96592f7c51ff9c34fb8d3
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2018-04-15 18:42:04 +0200

    testing digital inputs

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -15,7 +15,14 @@ run()
 kpro = Kpro()
 time = Time()
 odo = Odometer()
+di4 = DigitalInput(4)
+di17 = DigitalInput(17)
+di27 = DigitalInput(27)
 di22 = DigitalInput(22)
+di21 = DigitalInput(21)
+di6 = DigitalInput(6)
+di5 = DigitalInput(5)
+di12 = DigitalInput(12)
 while True:
     odo.save(kpro.vss())
     publish('com.app.idea', {'bat': kpro.bat(),
@@ -30,6 +37,14 @@ while True:
                              'an0': kpro.analog_input(0),
                              'time': time.get_time(),
                              'odo': odo.get_mileage(),
-                             'di22': di22.status()
-                             })
+                             'di4': di4.status(),
+                             'di17': di17.status(),
+                             'di27': di27.status(),
+                             'di22': di22.status(),
+                             'di21': di21.status(),
+                             'di6': di6.status(),
+                             'di5': di5.status(),
+                             'di12': di12.status(),
+                             }
+            )
     sleep(0.1)
