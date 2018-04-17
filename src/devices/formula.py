d8bdb17972339e5fe0b27b31883505d2c0d024f6
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2018-04-18 01:21:16 +0200

    added new formulas

--- src/devices/formula.py
+++ src/devices/formula.py
@@ -1,5 +1,6 @@
 class Formula:
-    # conversion for VDO 323-057 sensor powered by 4.8v and read with a 56ohms voltage divider
+
+    # VDO 323-057 sensor powered by 4.8v and read with a 56ohms voltage divider
     def vdo_323_057(adc):
         volts = (adc / 4096.000) * 4.80
         return (int)(-9.805174198 * pow(10, -1) * pow(volts, 9) + 23.4368155 * pow(volts, 8) - 240.7430517 * pow(volts,
@@ -8,7 +9,7 @@ class Formula:
                                                                                                       3) + 14423.41426 * pow(
             volts, 2) - 7152.975474 * volts + 1697.497838)
 
-    # conversion for VDO 360-004 sensor powered by 4.8v and read with a 56ohms voltage divider
+    # VDO 360-004 sensor powered by 4.8v and read with a 56ohms voltage divider
     def vdo_360_004(adc):
         volts = (adc/4096.000)*4.80
         return round(7.671035919*pow(10,-2)*pow(volts,7)-1.077184901*pow(volts,6)+6.295494139*pow(volts,5)-19.62567902*pow(volts,4)+35.08161116*pow(volts,3)-35.51613665*pow(volts,2)+19.52857924*volts-4.551671147,1)
@@ -16,3 +17,13 @@ class Formula:
     def fuel_civic_eg_tank(adc):
         volts = (adc / 4096.000) * 4.80
         return int(-7.348540077 * pow(10, -1) * pow(volts, 2) - 32.27276861 * volts + 109.170896)
+
+    # Autometer #2246 (4590-0023-12) 100 psi oil pressure sensor or ebay 100 psi
+    def autometer_2246(adc):
+        volts = (adc / 4096.000) * 5.0
+        return 25 * volts - 12.5
+
+    # ebay 150 psi oil pressure sensor
+    def ebay_150_psi(adc):
+        volts = (adc / 4096.000) * 5.0
+        return 37.5 * volts - 18.75
