eef11f92fc6ce18f7dd2b50377338501ea15ea83
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2018-04-18 22:51:46 +0200

    added vdo formula

--- src/devices/formula.py
+++ src/devices/formula.py
@@ -1,22 +1,10 @@
 class Formula:
-
-    # VDO 323-057 sensor powered by 4.8v and read with a 56ohms voltage divider
+    # VDO 323-057 sensor powered by 5v and read with a 56ohms voltage divider
     def vdo_323_057(adc):
-        volts = (adc / 4096.000) * 4.80
-        return (int)(-9.805174198 * pow(10, -1) * pow(volts, 9) + 23.4368155 * pow(volts, 8) - 240.7430517 * pow(volts,
-                                                                                                                 7) + 1390.11628 * pow(
-            volts, 6) - 4955.008229 * pow(volts, 5) + 11266.31187 * pow(volts, 4) - 16289.93484 * pow(volts,
-                                                                                                      3) + 14423.41426 * pow(
-            volts, 2) - 7152.975474 * volts + 1697.497838)
-
-    # VDO 360-004 sensor powered by 4.8v and read with a 56ohms voltage divider
-    def vdo_360_004(adc):
-        volts = (adc/4096.000)*4.80
-        return round(7.671035919*pow(10,-2)*pow(volts,7)-1.077184901*pow(volts,6)+6.295494139*pow(volts,5)-19.62567902*pow(volts,4)+35.08161116*pow(volts,3)-35.51613665*pow(volts,2)+19.52857924*volts-4.551671147,1)
-
-    def fuel_civic_eg_tank(adc):
-        volts = (adc / 4096.000) * 4.80
-        return int(-7.348540077 * pow(10, -1) * pow(volts, 2) - 32.27276861 * volts + 109.170896)
+        volts = (adc / 4096.000) * 5.0
+        return -6.556773822 * pow(10, -1) * pow(volts, 9) + 16.2933761 * pow(volts, 8) - 173.9768837 * pow(volts, 7) \
+               + 1044.151731 * pow(volts, 6) - 3868.010224 * pow(volts, 5) + 9139.591176 * pow(volts, 4) \
+               - 13733.10194 * pow(volts, 3) + 12638.52233 * pow(volts, 2) - 6520.045818 * volts + 1620.527214
 
     # Autometer #2246 (4590-0023-12) 100 psi oil pressure sensor or ebay 100 psi
     def autometer_2246(adc):
@@ -27,3 +15,7 @@ class Formula:
     def ebay_150_psi(adc):
         volts = (adc / 4096.000) * 5.0
         return 37.5 * volts - 18.75
+
+    def fuel_civic_eg_tank(adc):
+        volts = (adc / 4096.000) * 4.80
+        return int(-7.348540077 * pow(10, -1) * pow(volts, 2) - 32.27276861 * volts + 109.170896)
