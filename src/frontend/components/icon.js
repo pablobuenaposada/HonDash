6ad2c84a97daa5c650f14e303d71e994b7ac412c
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-04-19 13:34:00 +0200

    Refactor refresh conditionals to be more clear

--- src/frontend/components/icon.js
+++ src/frontend/components/icon.js
@@ -5,22 +5,15 @@ class Icon {
         this.path_off = path_off;
         this.path_on = path_on;
         var img = document.createElement("img");
-        img.src = "icons/"+this.path_off;
+        img.src = "icons/" + this.path_off;
         img.style.width = "100%";
         this.element.appendChild(img);
     }
 
     refresh(value){
-        if(value > 0){
-            var img = document.createElement("img");
-            img.src = "icons/"+this.path_on;
-            img.style.width = "100%";
-        }
-        else{
-            var img = document.createElement("img");
-            img.src = "icons/"+this.path_off;
-            img.style.width = "100%";
-        }
+        var img = document.createElement("img");
+        img.style.width = "100%";
+        img.src = "icons/" + (value > 0 ? this.path_on : this.path_off);
         this.element.innerHTML = '';
         this.element.appendChild(img);
     }
