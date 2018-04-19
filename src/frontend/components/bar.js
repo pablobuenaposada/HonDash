c5a5b53beb031056845800ce9c539c0f562e6d8b
Luis García Estrades <luisgarciaestrades@gmail.com>
2018-04-19 16:24:59 +0200

    Refactor Bar class

--- src/frontend/components/bar.js
+++ src/frontend/components/bar.js
@@ -1,10 +1,13 @@
 class Bar {
 
-     constructor(id, x, y, isVertical, backgroundColor, fillColor, minValue, maxValue, enableTextValue, textEnding, textFont, textWeight, textSize){
+    // @todo: refactor the constructor to pass an object
+    // and not a list of arguments
+    constructor(id, x, y, isVertical, backgroundColor, fillColor, minValue, maxValue, enableTextValue, textEnding, textFont, textWeight, textSize) {
+        var element = document.getElementById(id);
         this.x = x;
         this.y = y;
-        this.width = document.getElementById(id).offsetWidth;
-        this.height = document.getElementById(id).offsetHeight;
+        this.width = element.offsetWidth;
+        this.height = element.offsetHeight;
         this.isVertical = isVertical;
         this.minValue = minValue;
         this.maxValue = maxValue;
@@ -14,36 +17,60 @@ class Bar {
         this.textWeight = textWeight;
         this.textSize = textSize;
 
+        // Raphael paper object
         this.paper = Raphael(id, "100%", "100%");
 
+        // Background
         this.background = this.paper.rect(x, y, this.width, this.height);
-        this.background.attr({fill: backgroundColor, stroke: backgroundColor});
+        this.background.attr({
+            fill: backgroundColor,
+            stroke: backgroundColor,
+        });
 
+        // Bar fill & stroke
         this.bar = this.paper.rect(x, y, this.width, this.height);
-        this.bar.attr({fill: fillColor, stroke: fillColor});
+        this.bar.attr({
+            fill: fillColor,
+            stroke: fillColor,
+        });
 
-        this.text = this.paper.text((this.width/2)+x, y+(this.height/2), "");
-     }
+        // Center the text on the Bar
+        this.text = this.paper.text(
+            (this.width / 2) + x,
+            y + (this.height / 2),
+            ""
+        );
+    }
 
-    refresh(value){
-        if (value > this.maxValue) {
-            value = this.maxValue;
-        }
-        else if (value < this.minValue) {
-            value = this.minValue;
-        }
+    refresh(value) {
+        value = value > this.maxValue
+            ? this.maxValue
+            : (value < this.minValue)
+            ? this.minValue
+            : value;
 
-        if (this.isVertical) {
-            this.newWidth = (this.width * value) / this.maxValue;
-            this.bar.attr({width: this.newWidth});
-        }
-        else {
-            this.newHeight = (this.height * value) / this.maxValue;
-            this.bar.attr({y: (this.y - this.newHeight) + this.height  , height: this.newHeight});
-        }
+        var verticalAttr = function () {
+            return { width: this.width * value / this.maxValue }
+        };
 
-        if (this.enableTextValue) {
-            this.text.attr({text: value+this.textEnding, "font-family": this.textFont, "font-weight": this.textWeight, "font-size": this.textSize});
-        }
+        var horizontalAttr = function () {
+            var newHeight = this.height * value / this.maxValue;
+            return {
+                y: this.y - newHeight + this.height,
+                height: newHeight
+            }
+        };
+
+        this.bar.attr(this.isVertical
+            ? verticalAttr()
+            : horizontalAttr()
+        );
+
+        this.enableTextValue && this.text.attr({
+            "font-family": this.textFont,
+            "font-size": this.textSize
+            "font-weight": this.textWeight,
+            text: value + this.textEnding,
+        });
     }
 };
