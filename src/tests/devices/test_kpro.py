b2524944f457ccd917f73b57ae183b112a74d066
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2018-04-23 20:22:04 +0200

    add kpro tests

--- src/tests/devices/test_kpro.py
+++ src/tests/devices/test_kpro.py
@@ -5,7 +5,7 @@ from devices.kpro import Kpro
 
 class TestKpro(TestCase):
 
-    '''def setUp(self):
+    def setUp(self):
         self.kpro = Kpro()
         self.kpro.data0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -20,9 +20,9 @@ class TestKpro(TestCase):
         self.kpro.version = 4
         self.kpro.data0[2] = 100
         self.kpro.data0[3] = 100
-        self.assertEqual(self.kpro.rpm(), 6400.5)
+        self.assertEqual(self.kpro.rpm(), 6425)
 
     def test_tps_v4(self):
         self.kpro.version = 4
         self.kpro.data0[5] = 100
-        self.assertEqual(self.kpro.tps(), 37.98076923076923)'''
+        self.assertEqual(self.kpro.tps(), 37)
