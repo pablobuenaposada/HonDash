32dcab7292f208e333344249fb005441c5a16045
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2019-04-05 17:44:50 +0200

    killing tests warnings

--- src/devices/kpro/kpro.py
+++ src/devices/kpro/kpro.py
@@ -17,20 +17,22 @@ class Kpro:
         self.data3 = []
         self.data4 = []
         self.data5 = []
-        self.dev = None
+        self.kpro_device = None
         self.version = 0
 
-        while self.dev is None:
-            if usb.core.find(idVendor=0x403, idProduct=0xf5f8) is not None:  # kpro v2 & v3
-                self.dev = usb.core.find(idVendor=0x403, idProduct=0xf5f8)
-                self.version = 23
-            elif usb.core.find(idVendor=0x1c40, idProduct=0x0434) is not None:  # kpro v4
-                self.dev = usb.core.find(idVendor=0x1c40, idProduct=0x0434)
-                self.version = 4
-            if self.dev is not None:
+        # let's see if we can find a recognized kpro device
+        while self.kpro_device is None:
+            if usb.core.find(idVendor=constants.KPRO23_ID_VENDOR, idProduct=constants.KPRO23_ID_PRODUCT) is not None:  # kpro v2/3
+                self.kpro_device = usb.core.find(idVendor=constants.KPRO23_ID_VENDOR, idProduct=constants.KPRO23_ID_PRODUCT)
+                self.version = constants.KPRO23_ID
+            elif usb.core.find(idVendor=constants.KPRO4_ID_VENDOR, idProduct=constants.KPRO4_ID_PRODUCT) is not None:  # kpro v4
+                self.kpro_device = usb.core.find(idVendor=constants.KPRO4_ID_VENDOR, idProduct=constants.KPRO4_ID_PRODUCT)
+                self.version = constants.KPRO4_ID
+
+            if self.kpro_device is not None:  # if kpro device is found
                 try:
-                    self.dev.set_configuration()
-                    cfg = self.dev.get_active_configuration()
+                    self.kpro_device.set_configuration()
+                    cfg = self.kpro_device.get_active_configuration()
                     intf = cfg[(0, 0)]
                     self.ep = usb.util.find_descriptor(
                         intf,
@@ -38,8 +40,8 @@ class Kpro:
                     threading.Thread(target=self.update).start()
                 except usb.core.USBError:
                     # if there's an error while connecting to the usb device we just want to try again so let's ensure
-                    # that the while loop condition while go on
-                    self.dev = None
+                    # that we keep in the while loop condition
+                    self.kpro_device = None
 
     def update(self):
         usb_status = True
@@ -47,43 +49,43 @@ class Kpro:
             try:
                 assert self.ep is not None
 
-                if self.version == 4:
+                if self.version == constants.KPRO4_ID:
                     self.ep.write('\x40')
-                    self.data4 = self.dev.read(0x82, 1000)  # kpro v4
+                    self.data4 = self.kpro_device.read(0x82, 1000)  # kpro v4
                     self.ep.clear_halt()
 
                 self.ep.write('\x60')
-                if self.version == 23:
-                    self.data0 = self.dev.read(0x81, 1000)  # kpro v2 & v3
-                elif self.version == 4:
-                    self.data0 = self.dev.read(0x82, 1000)  # kpro v4
+                if self.version == constants.KPRO23_ID:
+                    self.data0 = self.kpro_device.read(0x81, 1000)  # kpro v2 & v3
+                elif self.version == constants.KPRO4_ID:
+                    self.data0 = self.kpro_device.read(0x82, 1000)  # kpro v4
 
                 self.ep.clear_halt()
 
                 self.ep.write('\x61')
                 # found on kpro2 that sometimes len=44, normally 16
-                if self.version == 23:
-                    self.data1 = self.dev.read(0x81, 1000)  # kpro v2 & v3
-                elif self.version == 4:
-                    self.data1 = self.dev.read(0x82, 1000)  # kpro v4
+                if self.version == constants.KPRO23_ID:
+                    self.data1 = self.kpro_device.read(0x81, 1000)  # kpro v2 & v3
+                elif self.version == constants.KPRO4_ID:
+                    self.data1 = self.kpro_device.read(0x82, 1000)  # kpro v4
 
                 self.ep.write('\x62')
-                if self.version == 23:
-                    temp = self.dev.read(0x81, 1000)  # kpro v2 & v3
+                if self.version == constants.KPRO23_ID:
+                    temp = self.kpro_device.read(0x81, 1000)  # kpro v2 & v3
                     if len(temp) == 68:
                         self.data2 = temp
-                elif self.version == 4:
-                    temp = self.dev.read(0x82, 1000)  # kpro v4
+                elif self.version == constants.KPRO4_ID:
+                    temp = self.kpro_device.read(0x82, 1000)  # kpro v4
                     if len(temp) == 25:
                         self.data2 = temp
 
-                if self.version == 4:
+                if self.version == constants.KPRO4_ID:
                     self.ep.write('\x65')
-                    self.data3 = self.dev.read(0x82, 128, 1000)  # kpro v4
+                    self.data3 = self.kpro_device.read(0x82, 128, 1000)  # kpro v4
                 else:  # for v3 only, v2 will not return anything meaningful
                     self.ep.clear_halt()
                     self.ep.write('\xb0')
-                    self.data5 = self.dev.read(0x81, 1000)
+                    self.data5 = self.kpro_device.read(0x81, 1000)
 
             except Exception:
                 # if there's an error while gathering the data, stop the update and try to reconnect usb again
@@ -91,26 +93,32 @@ class Kpro:
                 self.__init__()
 
     def bat(self):
+        """
+        Battery voltage
+        """
         # return unit: volts
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return self.data1[constants.KPRO23_BAT] * 0.1
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return self.data1[constants.KPRO4_BAT] * 0.1
         except IndexError:
             return 0
 
     def eth(self):
+        """
+        Ethanol content
+        """
         # return unit: per cent
         try:
-            if self.version == 4:
+            if self.version == constants.KPRO4_ID:
                 return self.data3[constants.KPRO4_ETH]
         except IndexError:
             return 0
 
     def flt(self):
         try:
-            if self.version == 4:
+            if self.version == constants.KPRO4_ID:
                 index = constants.KPRO4_FLT
             else:
                 return {'celsius': 0, 'fahrenheit': 0}
@@ -121,12 +129,15 @@ class Kpro:
             return 0
 
     def o2(self):
+        """
+        Oxygen sensor
+        """
         # return unit: afr and lambda
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 index_1 = constants.KPRO23_AFR2
                 index_2 = constants.KPRO23_AFR1
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 index_1 = constants.KPRO4_AFR2
                 index_2 = constants.KPRO4_AFR1
             else:
@@ -138,11 +149,14 @@ class Kpro:
             return {'afr': 0, 'lambda': 0}
 
     def tps(self):
+        """
+        Throttle position sensor
+        """
         # return unit: 0-100%
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return int(interp(self.data0[constants.KPRO23_TPS], [21, 229], [0, 100]))
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return int(interp(self.data0[constants.KPRO4_TPS], [21, 229], [0, 100]))
             else:
                 return 0
@@ -150,11 +164,14 @@ class Kpro:
             return 0
 
     def vss(self):
+        """
+        Vehicle speed sensor
+        """
         # return unit: km/h and mph
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 index = constants.KPRO23_VSS
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 index = constants.KPRO4_VSS
             else:
                 return {'kmh': 0, 'mph': 0}
@@ -167,9 +184,9 @@ class Kpro:
     def rpm(self):
         # return unit: revs. per minute
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return int(((256 * self.data0[constants.KPRO23_RPM2]) + self.data0[constants.KPRO23_RPM1]) * 0.25)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return int(((256 * self.data0[constants.KPRO4_RPM2]) + self.data0[constants.KPRO4_RPM1]) * 0.25)
         except IndexError:
             return 0
@@ -177,14 +194,17 @@ class Kpro:
     def cam(self):
         # return units: degree
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return (self.data0[constants.KPRO23_CAM] - 40) * 0.5
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return (self.data0[constants.KPRO4_CAM] - 40) * 0.5
         except IndexError:
             return 0
 
     def ect(self):
+        """
+        Engine coolant temperature
+        """
         # return units: celsius and fahrenheit
         temperature = [302, 302, 298, 294, 289, 285, 282, 278, 273, 269, 266, 262, 258, 253, 249, 246, 242, 239, 235,
                        231, 226, 222, 219, 215, 212, 208, 206, 203, 201, 199, 197, 194, 192, 190, 188, 185, 183, 181,
@@ -200,9 +220,9 @@ class Kpro:
                        -4, -5, -7, -9, -11, -13, -14, -18, -20, -22, -23, -25, -27, -31, -32, -34, -38, -40, -40, -40,
                        -40]
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 index = constants.KPRO23_ECT
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 index = constants.KPRO4_ECT
             else:
                 return {'celsius': 0, 'fahrenheit': 0}
@@ -213,6 +233,9 @@ class Kpro:
             return {'celsius': 0, 'fahrenheit': 0}
 
     def iat(self):
+        """
+        Intake air temperature
+        """
         # return units: celsius and fahrenheit
         temperature = [302, 302, 298, 294, 289, 285, 282, 278, 273, 269, 266, 262, 258, 253, 249, 246, 242, 239, 235,
                        231, 226, 222, 219, 215, 212, 208, 206, 203, 201, 199, 197, 194, 192, 190, 188, 185, 183, 181,
@@ -228,9 +251,9 @@ class Kpro:
                        -4, -5, -7, -9, -11, -13, -14, -18, -20, -22, -23, -25, -27, -31, -32, -34, -38, -40, -40, -40,
                        -40]
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 index = constants.KPRO23_IAT
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 index = constants.KPRO4_IAT
             else:
                 return {'celsius': 0, 'fahrenheit': 0}
@@ -242,7 +265,7 @@ class Kpro:
 
     def gear(self):
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 gear = self.data0[constants.KPRO23_GEAR]
             elif self.version == 4:
                 gear = self.data0[constants.KPRO4_GEAR]
@@ -257,11 +280,14 @@ class Kpro:
             return 'N'
 
     def eps(self):
+        """
+        Electric power steering
+        """
         mask = 0x20
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_EPS] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_EPS] & mask)
         except IndexError:
             return False
@@ -269,39 +295,48 @@ class Kpro:
     def scs(self):
         mask = 0x10
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_SCS] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_SCS] & mask)
         except IndexError:
             return False
 
     def rvslck(self):
+        """
+        Reverse gear lock
+        """
         mask = 0x01
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_RVSLCK] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_RVSLCK] & mask)
         except IndexError:
             return False
 
     def bksw(self):
+        """
+        Brake switch
+        """
         mask = 0x02
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_BKSW] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_BKSW] & mask)
         except IndexError:
             return False
 
     def acsw(self):
+        """
+        Aircon switch
+        """
         mask = 0x04
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_ACSW] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_ACSW] & mask)
         except IndexError:
             return False
@@ -309,39 +344,48 @@ class Kpro:
     def accl(self):
         mask = 0x08
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_ACCL] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_ACCL] & mask)
         except IndexError:
             return False
 
     def flr(self):
+        """
+        Fuel relay
+        """
         mask = 0x40
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_FLR] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_FLR] & mask)
         except IndexError:
             return False
 
     def fanc(self):
+        """
+        Fan switch
+        """
         mask = 0x80
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 return bool(self.data0[constants.KPRO23_FANC] & mask)
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 return bool(self.data0[constants.KPRO4_FANC] & mask)
         except IndexError:
             return False
 
     def map(self):
+        """
+        Manifold absolute pressure
+        """
         # return unit: bar, mbar and psi
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 index = constants.KPRO23_MAP
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 index = constants.KPRO4_MAP
             else:
                 return {'bar': 0, 'mbar': 0, 'psi': 0}
@@ -353,14 +397,17 @@ class Kpro:
             return {'bar': 0, 'mbar': 0, 'psi': 0}
 
     def mil(self):
+        """
+        Malfunction indicator light also known as check engine light
+        """
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 mil = self.data0[constants.KPRO23_MIL]
                 if mil == 9:
                     return True
                 elif mil == 1:
                     return False
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 mil = self.data3[constants.KPRO4_MIL]
                 if mil >= 36:
                     return True
@@ -373,9 +420,9 @@ class Kpro:
 
     def ecu_type(self):
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 type = self.data4[constants.KPRO23_ECU_TYPE]
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 type = self.data4[constants.KPRO4_ECU_TYPE]
             else:
                 return "unknown"
@@ -388,10 +435,13 @@ class Kpro:
             return "unknown"
 
     def ign(self):
+        """
+        Ignition timing
+        """
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 ign = self.data4[constants.KPRO23_IGN]
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 ign = self.data4[constants.KPRO4_IGN]
             else:
                 return False
@@ -405,10 +455,10 @@ class Kpro:
 
     def serial(self):
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 serial1 = self.data4[constants.KPRO23_SERIAL1]
                 serial2 = self.data4[constants.KPRO23_SERIAL2]
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 serial1 = self.data4[constants.KPRO4_SERIAL1]
                 serial2 = self.data4[constants.KPRO4_SERIAL2]
             else:
@@ -420,10 +470,10 @@ class Kpro:
 
     def firmware(self):
         try:
-            if self.version == 23:
+            if self.version == constants.KPRO23_ID:
                 firm1 = self.data4[constants.KPRO23_FIRM1]
                 firm2 = self.data4[constants.KPRO23_FIRM2]
-            elif self.version == 4:
+            elif self.version == constants.KPRO4_ID:
                 firm1 = self.data4[constants.KPRO4_FIRM1]
                 firm2 = self.data4[constants.KPRO4_FIRM2]
             else:
@@ -435,7 +485,7 @@ class Kpro:
 
     def analog_input(self, channel):
         # return unit: volts
-        if self.version == 4:
+        if self.version == constants.KPRO4_ID:
             if channel == 0:
                 index_1 = constants.KPRO4_AN0_1
                 index_2 = constants.KPRO4_AN0_2
@@ -468,7 +518,7 @@ class Kpro:
             except IndexError:
                 return 0
 
-        elif self.version == 23:
+        elif self.version == constants.KPRO23_ID:
             if channel == 0:
                 index_1 = constants.KPRO3_AN0_1
                 index_2 = constants.KPRO3_AN0_2
--- src/tests/devices/test_kpro.py
+++ src/tests/devices/test_kpro.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, mock
 
+from devices.kpro import constants
 from devices.kpro.kpro import Kpro
 
 
@@ -19,94 +20,94 @@ class TestKpro(TestCase):
                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     def test_battery_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data1[4] = 123
         self.assertEqual(self.kpro.bat(), 12.3)
 
     def test_rpm_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data0[2] = 100
         self.kpro.data0[3] = 100
         self.assertEqual(self.kpro.rpm(), 6425)
 
     def test_tps_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data0[5] = 100
         self.assertEqual(self.kpro.tps(), 37)
 
     def test_o2_v4_valid(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data0[16] = 0
         self.kpro.data0[17] = 128
-        self.assertEquals(self.kpro.o2()['afr'], 14.7)
-        self.assertEquals(self.kpro.o2()['lambda'], 1)
+        self.assertEqual(self.kpro.o2()['afr'], 14.7)
+        self.assertEqual(self.kpro.o2()['lambda'], 1)
 
     def test_o2_v23_valid(self):
-        self.kpro.version = 23
+        self.kpro.version = constants.KPRO23_ID
         self.kpro.data0[18] = 0
         self.kpro.data0[19] = 128
-        self.assertEquals(self.kpro.o2()['afr'], 14.7)
-        self.assertEquals(self.kpro.o2()['lambda'], 1)
+        self.assertEqual(self.kpro.o2()['afr'], 14.7)
+        self.assertEqual(self.kpro.o2()['lambda'], 1)
 
     def test_o2_v4_division_by_zero(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data0[18] = 0
         self.kpro.data0[19] = 0
         self.assertEqual(self.kpro.o2()['afr'], 0)
-        self.assertEquals(self.kpro.o2()['lambda'], 0)
+        self.assertEqual(self.kpro.o2()['lambda'], 0)
 
     def test_vss_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data0[4] = 100
-        self.assertEquals(self.kpro.vss()['kmh'], 100)
-        self.assertEquals(self.kpro.vss()['mph'], 62)
+        self.assertEqual(self.kpro.vss()['kmh'], 100)
+        self.assertEqual(self.kpro.vss()['mph'], 62)
 
     def test_vss_v23(self):
-        self.kpro.version = 23
+        self.kpro.version = constants.KPRO23_ID
         self.kpro.data0[6] = 100
-        self.assertEquals(self.kpro.vss()['kmh'], 100)
-        self.assertEquals(self.kpro.vss()['mph'], 62)
+        self.assertEqual(self.kpro.vss()['kmh'], 100)
+        self.assertEqual(self.kpro.vss()['mph'], 62)
 
     def test_ect_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data1[2] = 31
-        self.assertEquals(self.kpro.ect()['celsius'], 90)
-        self.assertEquals(self.kpro.ect()['fahrenheit'], 194)
+        self.assertEqual(self.kpro.ect()['celsius'], 90)
+        self.assertEqual(self.kpro.ect()['fahrenheit'], 194)
 
     def test_ect_v23(self):
-        self.kpro.version = 23
+        self.kpro.version = constants.KPRO23_ID
         self.kpro.data1[4] = 31
-        self.assertEquals(self.kpro.ect()['celsius'], 90)
-        self.assertEquals(self.kpro.ect()['fahrenheit'], 194)
+        self.assertEqual(self.kpro.ect()['celsius'], 90)
+        self.assertEqual(self.kpro.ect()['fahrenheit'], 194)
 
     def test_iat_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data1[3] = 31
-        self.assertEquals(self.kpro.iat()['celsius'], 90)
-        self.assertEquals(self.kpro.iat()['fahrenheit'], 194)
+        self.assertEqual(self.kpro.iat()['celsius'], 90)
+        self.assertEqual(self.kpro.iat()['fahrenheit'], 194)
 
     def test_iat_v23(self):
-        self.kpro.version = 23
+        self.kpro.version = constants.KPRO23_ID
         self.kpro.data1[5] = 31
-        self.assertEquals(self.kpro.iat()['celsius'], 90)
-        self.assertEquals(self.kpro.iat()['fahrenheit'], 194)
+        self.assertEqual(self.kpro.iat()['celsius'], 90)
+        self.assertEqual(self.kpro.iat()['fahrenheit'], 194)
 
     def test_map_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data0[6] = 100
-        self.assertEquals(self.kpro.map()['bar'], 1)
-        self.assertEquals(self.kpro.map()['mbar'], 1000)
-        self.assertEquals(self.kpro.map()['psi'], 14.503773773)
+        self.assertEqual(self.kpro.map()['bar'], 1)
+        self.assertEqual(self.kpro.map()['mbar'], 1000)
+        self.assertEqual(self.kpro.map()['psi'], 14.503773773)
 
     def test_map_v23(self):
-        self.kpro.version = 23
+        self.kpro.version = constants.KPRO23_ID
         self.kpro.data0[8] = 100
-        self.assertEquals(self.kpro.map()['bar'], 1)
-        self.assertEquals(self.kpro.map()['mbar'], 1000)
-        self.assertEquals(self.kpro.map()['psi'], 14.503773773)
+        self.assertEqual(self.kpro.map()['bar'], 1)
+        self.assertEqual(self.kpro.map()['mbar'], 1000)
+        self.assertEqual(self.kpro.map()['psi'], 14.503773773)
 
     def test_analog_input_v4(self):
-        self.kpro.version = 4
+        self.kpro.version = constants.KPRO4_ID
         self.kpro.data3[66] = 52
         self.kpro.data3[67] = 3
         self.kpro.data3[68] = 52
@@ -123,11 +124,11 @@ class TestKpro(TestCase):
         self.kpro.data3[79] = 3
         self.kpro.data3[80] = 52
         self.kpro.data3[81] = 3
-        self.assertEquals(self.kpro.analog_input(0), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(1), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(2), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(3), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(4), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(5), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(6), 1.0009765625)
-        self.assertEquals(self.kpro.analog_input(7), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(0), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(1), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(2), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(3), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(4), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(5), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(6), 1.0009765625)
+        self.assertEqual(self.kpro.analog_input(7), 1.0009765625)
