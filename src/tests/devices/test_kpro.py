83107994a559ba21edb47e077d7f1459b2f27b05
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2019-10-16 00:06:26 +0200

    improved kpro tests

--- src/tests/devices/test_kpro.py
+++ src/tests/devices/test_kpro.py
@@ -28,143 +28,10 @@ class TestKpro:
         with mock.patch("devices.kpro.kpro.Kpro.__init__") as m___init__:
             m___init__.return_value = None
             self.kpro = Kpro()
-        self.kpro.data0 = [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-        ]
-        self.kpro.data1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.kpro.data3 = [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-        ]
+        self.kpro.data0 = [None for _ in range(38)]
+        self.kpro.data1 = [None for _ in range(6)]
+        self.kpro.data3 = [None for _ in range(82)]
+        self.kpro.data4 = [None for _ in range(18)]
 
     def test_battery_v4(self):
         self.kpro.version = constants.KPRO4_ID
@@ -187,24 +54,24 @@ class TestKpro:
 
     def test_o2_v4_valid(self):
         self.kpro.version = constants.KPRO4_ID
-        self.kpro.data0[16] = 0
-        self.kpro.data0[17] = 128
+        self.kpro.data0[constants.KPRO4_AFR1] = 0
+        self.kpro.data0[constants.KPRO4_AFR2] = 128
 
         assert self.kpro.o2()["afr"] == 14.7
         assert self.kpro.o2()["lambda"] == 1
 
     def test_o2_v23_valid(self):
         self.kpro.version = constants.KPRO23_ID
-        self.kpro.data0[18] = 0
-        self.kpro.data0[19] = 128
+        self.kpro.data0[constants.KPRO23_AFR1] = 0
+        self.kpro.data0[constants.KPRO23_AFR2] = 128
 
         assert self.kpro.o2()["afr"] == 14.7
         assert self.kpro.o2()["lambda"] == 1
 
     def test_o2_v4_division_by_zero(self):
         self.kpro.version = constants.KPRO4_ID
-        self.kpro.data0[18] = 0
-        self.kpro.data0[19] = 0
+        self.kpro.data0[constants.KPRO4_AFR1] = 0
+        self.kpro.data0[constants.KPRO4_AFR2] = 0
 
         assert self.kpro.o2()["afr"] == 0
         assert self.kpro.o2()["lambda"] == 0
@@ -270,6 +137,160 @@ class TestKpro:
         assert self.kpro.map()["bar"] == 1
         assert self.kpro.map()["mbar"] == 1000
         assert self.kpro.map()["psi"] == 14.503773773
+
+    @pytest.mark.parametrize("kpro_value, result", ((100, 30.0),))
+    def test_cam_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_CAM] = kpro_value
+
+        assert self.kpro.cam() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((100, 30.0),))
+    def test_cam_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_CAM] = kpro_value
+
+        assert self.kpro.cam() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, "N"), (1, 1)))
+    def test_gear_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_GEAR] = kpro_value
+
+        assert self.kpro.gear() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, "N"), (1, 1)))
+    def test_gear_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_GEAR] = kpro_value
+
+        assert self.kpro.gear() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (32, True)))
+    def test_eps_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_EPS] = kpro_value
+
+        assert self.kpro.eps() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (32, True)))
+    def test_eps_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_EPS] = kpro_value
+
+        assert self.kpro.eps() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (16, True)))
+    def test_scs_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_SCS] = kpro_value
+
+        assert self.kpro.scs() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (16, True)))
+    def test_scs_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_SCS] = kpro_value
+
+        assert self.kpro.scs() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (1, True)))
+    def test_rvslck_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_RVSLCK] = kpro_value
+
+        assert self.kpro.rvslck() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (1, True)))
+    def test_rvslck_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_RVSLCK] = kpro_value
+
+        assert self.kpro.rvslck() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (2, True)))
+    def test_bksw_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_BKSW] = kpro_value
+
+        assert self.kpro.bksw() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (2, True)))
+    def test_bksw_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_BKSW] = kpro_value
+
+        assert self.kpro.bksw() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (4, True)))
+    def test_acsw_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_ACSW] = kpro_value
+
+        assert self.kpro.acsw() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (4, True)))
+    def test_acsw_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_ACSW] = kpro_value
+
+        assert self.kpro.acsw() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (8, True)))
+    def test_accl_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_ACCL] = kpro_value
+
+        assert self.kpro.accl() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (8, True)))
+    def test_accl_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_ACCL] = kpro_value
+
+        assert self.kpro.accl() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (64, True)))
+    def test_flr_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_FLR] = kpro_value
+
+        assert self.kpro.flr() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (64, True)))
+    def test_flr_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_FLR] = kpro_value
+
+        assert self.kpro.flr() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (128, True)))
+    def test_fanc_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data0[constants.KPRO23_FANC] = kpro_value
+
+        assert self.kpro.fanc() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (128, True)))
+    def test_fanc_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data0[constants.KPRO4_FANC] = kpro_value
+
+        assert self.kpro.fanc() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (1, True)))
+    def test_ign_v23(self, kpro_value, result):
+        self.kpro.version = constants.KPRO23_ID
+        self.kpro.data4[constants.KPRO23_IGN] = kpro_value
+
+        assert self.kpro.ign() == result
+
+    @pytest.mark.parametrize("kpro_value, result", ((0, False), (1, True)))
+    def test_ign_v4(self, kpro_value, result):
+        self.kpro.version = constants.KPRO4_ID
+        self.kpro.data4[constants.KPRO4_IGN] = kpro_value
+
+        assert self.kpro.ign() == result
 
     def test_analog_input_v4(self):
         self.kpro.version = constants.KPRO4_ID
