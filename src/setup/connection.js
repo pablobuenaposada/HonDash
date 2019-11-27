edba7c905881846b7c8eb1eabd65ea76adc6b031
Pablo <pablo.buenaposada@gmail.com>
2019-11-27 13:35:40 +0100

    add macos system requirements in makefile
    closes #71
    
    force units
    
    force units

--- src/setup/connection.js
+++ src/setup/connection.js
@@ -101,9 +101,119 @@ function checkDivColor() {
   }
 }
 
+function checkUnitValues() {
+
+  var formulas =
+  "contains(., '[an') and contains(., '[formula]')";
+
+  var tempUnits = ["celsius", "fahrenheit"];
+  var pressureUnits = ["psi", "bar"];
+  var otherUnits = ["per cent"];
+
+  var divs = getElementsByXPath(
+    "//*[@id='editor_holder']/div/div[@class='well well-sm']/div/div/*[@class='row']/div/div[@class='well well-sm']"
+  );
+
+  for (var div in divs) {
+
+    var formula = getElementsByXPath(
+      "div/div/*[@class='row']/div[@data-schemapath[starts-with(., 'root.')]]/div/select[@name["+formulas+"]]",
+      divs[div]
+    );
+    if (formula.length > 0) {
+        var selectedFormula = formula[0].options[formula[0].selectedIndex].value;
+        var unitsSelect = getElementsByXPath("div/div/*[@class='row']/div[@data-schemapath[starts-with(., 'root.')]]/div/select[@name[contains(., '[unit]')]]", divs[div]);
+
+        if (selectedFormula == "vdo_323_057"){
+            var op = unitsSelect[0].getElementsByTagName("option");
+            for (var i = 0; i < op.length; i++) {
+                if (tempUnits.includes(op[i].value)){
+                    op[i].disabled = false;
+                    unitsSelect[0].selectedIndex = i;
+                }
+                else{
+                    op[i].disabled = true;
+                }
+            }
+        }
+
+        else if (selectedFormula == "autometer_2246"){
+            var op = unitsSelect[0].getElementsByTagName("option");
+            for (var i = 0; i < op.length; i++) {
+                if (pressureUnits.includes(op[i].value)){
+                    op[i].disabled = false;
+                    unitsSelect[0].selectedIndex = i;
+                }
+                else{
+                    op[i].disabled = true;
+                }
+            }
+        }
+
+        else if (selectedFormula == "aem_30_2012"){
+            var op = unitsSelect[0].getElementsByTagName("option");
+            for (var i = 0; i < op.length; i++) {
+                if (tempUnits.includes(op[i].value)){
+                    op[i].disabled = false;
+                    unitsSelect[0].selectedIndex = i;
+                }
+                else{
+                    op[i].disabled = true;
+                }
+            }
+        }
+
+        else if (selectedFormula == "ebay_150_psi"){
+            var op = unitsSelect[0].getElementsByTagName("option");
+            for (var i = 0; i < op.length; i++) {
+                if (pressureUnits.includes(op[i].value)){
+                    op[i].disabled = false;
+                    unitsSelect[0].selectedIndex = i;
+                }
+                else{
+                    op[i].disabled = true;
+                }
+            }
+        }
+
+        else if (selectedFormula == "bosch_0280130039_0280130026"){
+            var op = unitsSelect[0].getElementsByTagName("option");
+            for (var i = 0; i < op.length; i++) {
+                if (tempUnits.includes(op[i].value)){
+                    op[i].disabled = false;
+                    unitsSelect[0].selectedIndex = i;
+                }
+                else{
+                    op[i].disabled = true;
+                }
+            }
+        }
+
+        else {
+            var op = unitsSelect[0].getElementsByTagName("option");
+            for (var i = 0; i < op.length; i++) {
+                if (otherUnits.includes(op[i].value)){
+                    op[i].disabled = false;
+                    unitsSelect[0].selectedIndex = i;
+                }
+                else{
+                    op[i].disabled = true;
+                }
+            }
+        }
+    }
+
+
+
+  }
+
+
+}
+
 function updateFields() {
   checkDivColor();
   checkTagValues();
+  checkUnitValues();
 }
 
 function download() {
