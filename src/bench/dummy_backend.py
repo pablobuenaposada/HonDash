87342b328c880ecf17d1643dfa3d3185c1a908e3
Pablo Buenaposada <pablo.buenaposada@gmail.com>
2020-03-05 21:25:56 +0100

    fix dummy makefile command

--- src/bench/dummy_backend.py
+++ src/bench/dummy_backend.py
@@ -1,69 +1,22 @@
 import random
 from time import sleep
+from unittest import mock
 
-from autobahn_sync import publish, run
+from backend.backend import Backend
 
-from devices.formula import Formula
-from devices.odometer import Odometer
-from devices.time import Time
+with mock.patch("usb.core.find"), mock.patch(
+    "devices.kpro.kpro.Kpro.__init__"
+) as m___init__:
+    m___init__.return_value = None
+    backend = Backend.get()
+    backend.kpro.status = True
+    backend.kpro.version = 4
 
-run()
-
-time = Time()
-odo = Odometer()
-while True:
-    odo.save(random.randint(0, 200))
-    publish(
-        "com.app.idea",
-        {
-            "bat": random.uniform(0, 15),
-            "map": {
-                "bar": random.uniform(0, 2),
-                "mbar": random.uniform(0, 2),
-                "psi": random.uniform(0, 2),
-            },
-            "gear": random.randint(0, 6),
-            "iat": {
-                "celsius": random.randint(0, 50),
-                "fahrenheit": random.randint(0, 50),
-            },
-            "tps": random.randint(0, 100),
-            "ect": {
-                "celsius": random.randint(0, 50),
-                "fahrenheit": random.randint(0, 50),
-            },
-            "rpm": random.randint(0, 9000),
-            "vss": {"kmh": random.randint(0, 200), "mph": random.randint(0, 200)},
-            "o2": {"afr": random.uniform(0, 20), "lambda": random.uniform(0, 1)},
-            "cam": random.randint(0, 50),
-            "mil": random.choice([True, False]),
-            "bksw": random.choice([True, False]),
-            "an0": Formula.psi_to_bar(Formula.ebay_150_psi(random.uniform(0, 5))),
-            "an1": Formula.autometer_2246(random.uniform(0, 5)),
-            "an2": random.uniform(0, 5),
-            "an3": random.uniform(0, 5),
-            "an4": random.uniform(0, 5),
-            "an5": random.uniform(0, 5),
-            "an6": random.uniform(0, 5),
-            "an7": random.uniform(0, 5),
-            "di4": random.randint(0, 1),
-            "di5": random.randint(0, 1),
-            "di6": random.randint(0, 1),
-            "di12": random.randint(0, 1),
-            "di21": random.randint(0, 1),
-            "di17": random.randint(0, 1),
-            "di27": random.randint(0, 1),
-            "di22": random.randint(0, 1),
-            "time": time.get_time(),
-            "odo": odo.get_mileage(),
-            "ai0": Formula.vdo_323_057(random.uniform(0, 5)),
-            "ai1": Formula.civic_eg_fuel_tank(random.uniform(0, 5)),
-            "ai2": random.uniform(0, 5),
-            "ai3": random.uniform(0, 5),
-            "ai4": random.uniform(0, 5),
-            "ai5": random.uniform(0, 5),
-            "ai6": random.uniform(0, 5),
-            "ai7": random.uniform(0, 5),
-        },
-    )
-    sleep(0.1)
+    while True:
+        backend.kpro.data0 = [random.randint(0, 255) for _ in range(38)]
+        backend.kpro.data1 = [random.randint(0, 255) for _ in range(7)]
+        backend.kpro.data3 = [random.randint(0, 255) for _ in range(100)]
+        backend.kpro.data4 = [random.randint(0, 255) for _ in range(18)]
+        backend.kpro.data5 = [random.randint(0, 255) for _ in range(20)]
+        backend.update()
+        sleep(0.1)  # TODO: not use sleep :(
