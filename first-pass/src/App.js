e9d382fc7dc4500d659032031aa1b3faf1ad41e0
AdamRJonesHPD <ajones@hra.com>
2021-03-12 12:57:00 -0800

    revert to base react app

--- first-pass/src/App.js
+++ first-pass/src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Gauge from './components/Gauge';
+import Icon from './components/Icon';
+import Bar from './components/Bar';
+import Speed from './components/Speed';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div class="w-full h-1"></div>
+
+          <div class="flex h-24">
+                <div class="w-1/6"></div>
+                <Bar/>
+                <div class="w-1/6"></div>
+            </div>
+          <div class="flex italic">
+            <Speed/>
+          </div>
+
+          <div class="grid grid-cols-6 gap-2 text-center overflow-hidden" style={{padding: "0 5px 0"}}>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+            <div><Gauge/></div>
+        </div>
+
+          <div><Icon/></div>
+
+          <div class="w-full h-3 text-center" style={{border: "1px solid red"}}>kpro version</div>
+
       </header>
     </div>
   );
